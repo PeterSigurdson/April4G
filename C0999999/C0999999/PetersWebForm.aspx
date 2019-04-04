5850a1e21f1b5f7d38446a945ec073f946cc51f3
petersigurdson <peter@petersigurdson.net>
2019-04-04 16:33:02 -0400

    4:30 checkpoint commit

---
+++ C0999999/C0999999/PetersWebForm.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PetersWebForm.aspx.cs" Inherits="C0999999.PetersWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Newsletter Signup</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <H1>Newsletter Signup</H1>
+        </div>
+        <asp:Label ID="lblEMAIL" runat="server" Font-Names="Arial Narrow" Text="Email Peter"></asp:Label>
+    </form>
+</body>
+</html>
--- C0999999/C0999999/PetersWebForm.aspx.designer.cs
+++ C0999999/C0999999/PetersWebForm.aspx.designer.cs
@@ -10,15 +10,24 @@
 namespace C0999999 {
     
     
-    public partial class SiteMaster {
+    public partial class PetersWebForm {
         
         /// <summary>
-        /// MainContent control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEMAIL;
     }
 }
